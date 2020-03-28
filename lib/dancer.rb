9ff8abf5130aa494c2ebbc08603b1820032ebf5d
Ramsey Chapin <ramsey@ramseychapin.com>
2020-03-28 22:09:17 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,15 +1,25 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+<<<<<<< HEAD
 require_relative './fancy_dance.rb'
 class Dancer
   #include Dance
   #extend MetaDancing
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
+=======
+class Dancer
+  extend MetaDancing
+  include Dance
+>>>>>>> dba372d822733608a2493f6b13773360ab60f0a9
 
   attr_accessor :name
 
   def initialize(name)
     @name = name
   end
+<<<<<<< HEAD
+=======
+
+>>>>>>> dba372d822733608a2493f6b13773360ab60f0a9
 end
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,5 +1,6 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+<<<<<<< HEAD
 require_relative './fancy_dance.rb'
 
 class Kid
@@ -7,6 +8,12 @@ class Kid
   #extend MetaDancing
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
+=======
+
+class Kid
+  extend MetaDancing
+  include Dance
+>>>>>>> dba372d822733608a2493f6b13773360ab60f0a9
 
   attr_accessor :name
 
